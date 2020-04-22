072ed41036ab88bf138a4af2754a7d9905e7c11c
Candace Wilson <candaceyw@gmail.com>
2020-04-21 21:43:09 -0400

    guided complete with notes

--- messages/messages-router.js
+++ messages/messages-router.js
@@ -1,26 +1,69 @@
-const express = require("express")
-const db = require("../data/config")
+const express = require('express');
+const db = require('../data/config');
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/", (req, res, next) => {
+router.get('/', async (req, res, next) => {
+	try {
+		// translates to SELECT * FROM "messages";
+		const messages = await db.select('*').from('messages');
+		res.json(messages);
+	} catch (error) {
+		next(error);
+	}
+});
 
-})
+router.get('/:id', async (req, res, next) => {
+	try {
+		// SELECT * FROM "messages" WHERE "id" = ? LIMIT 1;
+		// const [message] = await db.select('*').from('messages').where('id', req.params.id).limit(1);;
+		const message = await db('messages').where('id', req.params.id).first();
+		res.json(message);
+	} catch (error) {
+		next(error);
+	}
+});
 
-router.get("/:id", (req, res, next) => {
+router.post('/', async (req, res, next) => {
+	try {
+		const payload = {
+			title: req.body.title,
+			contents: req.body.contents,
+		};
+		// INSERT INTO "messages" ("title", "contents") VALUES (?, ?)
+		const [id] = await db('messages').insert(payload);
+		const message = await db('messages').where('id', id).first();
+		res.json(message);
+	} catch (error) {
+		next(error);
+	}
+});
 
-})
+router.put('/:id', async (req, res, next) => {
+	try {
+		const payload = {
+			title: req.body.title,
+			contents: req.body.contents,
+		};
+		// UPDATE "messages" SET "title" = ? AND "contents" = ? WHERE "id" = ?
+		await db('messages').where('id', req.params.id).update(payload);
+		const updatedMessage = await db('messages')
+			.where('id', req.params.id)
+			.first();
+		res.json(updatedMessage);
+	} catch (error) {
+		next(error);
+	}
+});
 
-router.post("/", (req, res, next) => {
+router.delete('/:id', async (req, res, next) => {
+	try {
+		// DELETE FROM "messages" WHERE "id"=?;
+		await db('messages').where('id', req.params.id).del();
+		res.status(204).end();
+	} catch (error) {
+		next(error);
+	}
+});
 
-})
-
-router.put("/:id", (req, res, next) => {
-
-})
-
-router.delete("/:id", (req, res, next) => {
-
-})
-
-module.exports = router
+module.exports = router;
